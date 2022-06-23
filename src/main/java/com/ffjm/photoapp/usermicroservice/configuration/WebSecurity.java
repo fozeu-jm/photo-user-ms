8f2e3d827134d79f815fbe845de6a8c6d3f8e213
FOZEU <fozeu.jm@gmail.com>
2022-06-23 23:48:21 +0200

    xml support and web security gateway ip configuration

--- src/main/java/com/ffjm/photoapp/usermicroservice/configuration/WebSecurity.java
+++ src/main/java/com/ffjm/photoapp/usermicroservice/configuration/WebSecurity.java
@@ -1,7 +1,9 @@
 package com.ffjm.photoapp.usermicroservice.configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -11,11 +13,18 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class WebSecurity {
 
+    private final Environment environment;
+
+    @Autowired
+    public WebSecurity(Environment environment) {
+        this.environment = environment;
+    }
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         //disable csrf on api
         http.csrf().disable();
-        http.authorizeRequests().antMatchers("/api/users/**").permitAll();
+        http.authorizeRequests().antMatchers("/**").hasIpAddress(environment.getProperty("gateway.ip"));
         return http.build();
     }
 }
--- src/main/java/com/ffjm/photoapp/usermicroservice/controllers/UsersController.java
+++ src/main/java/com/ffjm/photoapp/usermicroservice/controllers/UsersController.java
@@ -8,6 +8,7 @@ import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -31,7 +32,10 @@ public class UsersController {
         return "Working !!";
     }
 
-    @PostMapping
+    @PostMapping(
+            consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE},
+            produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE}
+    )
     public ResponseEntity<UserReponseModel> creatUser(@Valid @RequestBody UserRequestModel userRequestModel){
         ModelMapper modelMapper = new ModelMapper();
         modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
