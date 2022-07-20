fece1bc9db3200fea8ae9d1a31d1a87de6e7bcd2
FOZEU <fozeu.jm@gmail.com>
2022-07-20 23:47:21 +0200

    use resilience4j retry feature

--- src/main/java/com/ffjm/photoapp/usermicroservice/clients/PhotoServiceClient.java
+++ src/main/java/com/ffjm/photoapp/usermicroservice/clients/PhotoServiceClient.java
@@ -2,6 +2,7 @@ package com.ffjm.photoapp.usermicroservice.clients;
 
 import com.ffjm.photoapp.usermicroservice.clients.model.PhotoResponseModel;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,7 +14,8 @@ import java.util.List;
 public interface PhotoServiceClient {
 
     @GetMapping("/photo/userss/{userId}")
-    @CircuitBreaker(name = "user-ms", fallbackMethod = "getPhotoFallback")
+    @Retry(name = "photo-ms")
+    @CircuitBreaker(name = "photo-ms", fallbackMethod = "getPhotoFallback")
     List<PhotoResponseModel> getUsersPhoto(@PathVariable String userId);
 
     default List<PhotoResponseModel> getPhotoFallback(String userId, Throwable exc) {
