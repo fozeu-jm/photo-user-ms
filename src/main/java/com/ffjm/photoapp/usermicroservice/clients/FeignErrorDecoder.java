ef7899f75e0d0985ef754a4a3c62178cc58cb47e
FOZEU <fozeu.jm@gmail.com>
2022-07-20 22:14:47 +0200

    use resilience4j circuit breaker feature

--- src/main/java/com/ffjm/photoapp/usermicroservice/clients/FeignErrorDecoder.java
+++ src/main/java/com/ffjm/photoapp/usermicroservice/clients/FeignErrorDecoder.java
@@ -2,14 +2,19 @@ package com.ffjm.photoapp.usermicroservice.clients;
 
 import feign.Response;
 import feign.codec.ErrorDecoder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ResponseStatusException;
 
 @Component
 public class FeignErrorDecoder implements ErrorDecoder {
+    Logger logger = LoggerFactory.getLogger(FeignErrorDecoder.class);
+
     @Override
     public Exception decode(String methodKey, Response response) {
+
         switch (response.status()) {
             case 400:
                 return new ResponseStatusException(HttpStatus.valueOf(response.status()), "Bad request");
@@ -18,6 +23,7 @@ public class FeignErrorDecoder implements ErrorDecoder {
                 if (methodKey.contains("getUsersPhoto")) {
                     message = "Users photo  not found";
                 }
+                logger.error(new ResponseStatusException(HttpStatus.valueOf(response.status()), message).getLocalizedMessage());
                 return new ResponseStatusException(HttpStatus.valueOf(response.status()), message);
             default:
                 return new Exception(response.reason());
--- src/main/java/com/ffjm/photoapp/usermicroservice/clients/PhotoServiceClient.java
+++ src/main/java/com/ffjm/photoapp/usermicroservice/clients/PhotoServiceClient.java
@@ -1,15 +1,22 @@
 package com.ffjm.photoapp.usermicroservice.clients;
 
 import com.ffjm.photoapp.usermicroservice.clients.model.PhotoResponseModel;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @FeignClient(name = "photo-ms")
 public interface PhotoServiceClient {
 
-    @GetMapping("/photo/users/{userId}")
+    @GetMapping("/photo/userss/{userId}")
+    @CircuitBreaker(name = "user-ms", fallbackMethod = "getPhotoFallback")
     List<PhotoResponseModel> getUsersPhoto(@PathVariable String userId);
+
+    default List<PhotoResponseModel> getPhotoFallback(String userId, Throwable exc) {
+        return new ArrayList<>();
+    }
 }
